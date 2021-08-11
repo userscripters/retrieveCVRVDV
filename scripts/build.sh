0d292150de54eb6c4766e09719bdcd24ae502586
Oleg Valter <34833340+Oaphi@users.noreply.github.com>
2021-08-11 14:44:31 +0300

    Initial commit

---
+++ scripts/build.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+npm run prebuild &&
+    npm run build-legacy &&
+    npm run build-modern &&
+    npm run headers-tm &&
+    npm run readme
+
+declare files=$(find dist -iname "*\.js" -not -iname "headers*")
+
+for file in ${files}; do
+    mv "$file" "${file%.js}.user.js"
+done
---
+++ scripts/headers.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+dist="dist"
+output=$dist"/headers.js"
+
+generate tampermonkey \
+    -o $output \
+    -m $(cat .matches)
+
+userscript="$(find -iwholename "./$dist/*\.js" -type f -not -iname "*headers\.js")"
+
+sed -i -e "{1e cat $output; echo; echo" -e "; N}" $userscript
